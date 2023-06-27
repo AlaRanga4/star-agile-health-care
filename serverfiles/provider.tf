bfa2aa54df2f281e066d6553b3818baa0c9a19e8
AlaRanga4 <111878804+AlaRanga4@users.noreply.github.com>
2023-06-27 09:53:57 +0530

    Update provider.tf

--- serverfiles/provider.tf
+++ serverfiles/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "us-east-2"
-  access_key = ""
-  secret_key = ""
+  region     = "us-northeast-1"
+  access_key = "AKIAT7SRUDSYJW7HGXN7"
+  secret_key = "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"
 }
